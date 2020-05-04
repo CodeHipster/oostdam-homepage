b836c6f7c255b96f38d792eecd91ff52ed4fc976
codehipster <oostdam@gmail.com>
2020-05-04 13:49:11 +0200

    selectable menu

---
+++ public/static/navigation/navigation-item/navigation-item.js
@@ -0,0 +1,18 @@
+import addCss from '../../add-css.js'
+
+export default {
+    beforeCreate: function () {
+        addCss('static/navigation/navigation-item/navigation-item.css');
+    },
+    props: [
+        'title',
+        'isActive'
+    ],
+    methods: {
+        click: function () {
+            this.$emit('navigate', this.title)
+        }
+    },
+    template: `
+    <div class=navigation-item v-bind:class="{ selected: isActive }" @click="click">{{title}}</div>`
+}
--- public/static/navigation/navigation.js
+++ public/static/navigation/navigation.js
@@ -1,16 +1,33 @@
 import addCss from '../add-css.js'
+import navigationItem from './navigation-item/navigation-item.js'
 
 export default {
     beforeCreate: function () {
         addCss('static/navigation/navigation.css');
     },
+    data: function () {
+        return {
+            items: [
+                'my kiteboarding',
+                'my software',
+                'my resume',
+            ],
+            activeItem: 'my kiteboarding',
+        }
+    },
+    components: {
+        'navigation-item': navigationItem
+    },
+    methods:{
+        navigate: function(item){
+            this.activeItem = item;
+        }  
+    },
     template: `
     <div class=navigation>
         <div class=box>
-            <div class="item name">Thijs Oostdam</div>
-            <div class="item selected">my kiteboarding</div>
-            <div class="item">my software</div>
-            <div class="item">my resume</div>
+            <div class="name">Thijs Oostdam</div>
+            <navigation-item v-for="item in items" :key="item" :title="item" :isActive="item == activeItem" @navigate="navigate"></navigation-item>
         </div>
     </div>`
 }
