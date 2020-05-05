c3c536e4ec53fa2d64990ea4632f16f54b3b1940
codehipster <oostdam@gmail.com>
2020-05-05 15:51:22 +0200

    avoid double adding of css

--- public/static/add-css.js
+++ public/static/add-css.js
@@ -4,11 +4,14 @@
  * @param {*} fileName: path to the css file from the root of the app. e.g. static/oostdam.css
  */
 export default function(fileName, media = 'all') {
-    var head = document.getElementsByTagName('head')[0];
-    var style = document.createElement('link');
-    style.href = fileName;
-    style.type = 'text/css';
-    style.rel = 'stylesheet';
-    style.media = media;
-    head.append(style);
+    var existing = document.querySelectorAll('link[href="'+fileName+'"]');
+    if(existing.length == 0) {
+      var head = document.getElementsByTagName('head')[0];
+      var style = document.createElement('link');
+      style.href = fileName;
+      style.type = 'text/css';
+      style.rel = 'stylesheet';
+      style.media = media;
+      head.append(style);
+    }
   }
