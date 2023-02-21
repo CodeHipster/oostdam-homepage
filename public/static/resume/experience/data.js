35d64aee57bceb4f98abbc4a312d4bff3fdf9efb
Thijs Oostdam <thijs.oostdam@nn.nl>
2023-02-21 10:26:01 +0100

    update resume

--- public/static/resume/experience/data.js
+++ public/static/resume/experience/data.js
@@ -1,97 +1,114 @@
 export default {
-    courses: [{
-        date: '2017',
-        names: [
-            'Java architecture',
-            'Java design patterns',
-            'Blockchain: Ethereum developer'
-        ]
+  courses: [
+    {
+      date: '2020',
+      names:[
+        'VMware Spring Professional'
+      ]
     },
     {
-        date: '2014-2016',
-        names: [
-            'JavaScript',
-            'Node',
-            'Git'
-        ]
+      date: '2017',
+      names: [
+        'Java architecture',
+        'Java design patterns',
+        'Blockchain: Ethereum developer'
+      ]
     },
     {
-        date: '2013',
-        names: [
-            'Java 7 Programmer I&II'
-        ]
+      date: '2014-2016',
+      names: [
+        'JavaScript',
+        'Node',
+        'Git'
+      ]
+    },
+    {
+      date: '2013',
+      names: [
+        'Java 7 Programmer I&II'
+      ]
     }],
-    studies: [{
-        date: '2010-2012',
-        names: [
-            'IGAD\n(International Game Architecture and Design)'
-        ]
+  studies: [{
+    date: '2010-2012',
+    names: [
+      'IGAD\n(International Game Architecture and Design)'
+    ]
+  },
+  {
+    date: '2004-2010',
+    names: [
+      'Constructional Engineering\n(The Hague University of Applied Sciences)'
+    ]
+  }],
+  jobs: [
+    {
+      period: { from: 'June. 2020', to: 'current' },
+      company: 'NN',
+      role: 'DevOps Engineer',
+      responsibilities: 'Senior engineer of the team responsible for development and operations of the internet savings products and various other applications. But also educating fellow colleagues through workshops and knowledge sharing sessions.',
     },
     {
-        date: '2004-2010',
-        names: [
-            'Constructional Engineering\n(The Hague University of Applied Sciences)'
-        ]
-    }],
-    jobs: [
-        {
-            period: { from: 'Jan. 2020', to: 'Mar. 2020' },
-            company: 'ING',
-            role: 'Java Developer',
-            responsibilities: 'Ensure the static reference data team reaches their QBR targets by implementing a cached API and building a generic interface for receiving static data.',
-        },
-        {
-            period: { from: 'Dec. 2017', to: 'Dec. 2019' },
-            company: 'Rabobank',
-            role: 'Devops Engineer',
-            responsibilities: 'Develop maintain and design an application, in a scrum team of 7, which gives the customer insight into their running bank processes. This application was the first to go live on the cloud (Pivotal) and was set up with continuous delivery without downtime.',
-        },
-        {
-            period: { from: 'May 2016', to: 'Nov. 2017' },
-            company: 'EBPI',
-            role: 'Java Developer',
-            responsibilities: 'Develop and maintain, in a scrum team of 5, the platform used to send millions of messages between businesses and the government. As well as implementing the backend services for the governments "BerichtenBox".',
-        },
-        {
-            period: { from: 'Oct. 2014', to: 'Apr. 2016' },
-            company: 'SnappCar',
-            role: 'C# Developer',
-            responsibilities: 'Building and maintaining every aspect of the Snappcar website in a scrum team of 4 including a migration from a local machine to the Amazon cloud.',
-        }
-    ],
-    skills: {
-        hard: [
-            { name: 'Java', level: 100 },
-            { name: 'Dart', level: 30 },
-            { name: 'Flutter', level: 40 },
-            { name: 'Truffle suite', level: 30 },
-            { name: 'Blockchain', level: 50 },
-            { name: 'Spring Boot', level: 100 },
-            { name: 'Maven', level: 80 },
-            { name: 'Sonar', level: 80 },
-            { name: 'Jenkins', level: 80 },
-            { name: 'CICD', level: 80 },
-            { name: 'Micro services', level: 90 },
-            { name: 'TypeScript', level: 75 },
-            { name: 'Vue.js', level: 75 },
-            { name: 'Flexbox', level: 75 },
-            { name: 'HTML', level: 75 },
-            { name: 'WebdriverIO', level: 70 },
-            { name: 'REST', level: 95 },
-            { name: 'AWS', level: 60 },
-            { name: 'Cloud Foundry', level: 80 },
-            { name: 'SQL', level: 100 },
-            { name: 'Mongo', level: 80 },
-            { name: 'Redis', level: 90 },
-            { name: 'GA', level: 60 },
-            { name: 'Grafana', level: 70 },
-            { name: 'Prometheus', level: 70 },
-            { name: 'Web security', level: 60 },
-        ],
-        soft: [
-            { name: 'Agile', level: 90 },
-            { name: 'Team player', level: 90 },
-            { name: 'Creative', level: 90 }
-        ]
+      period: { from: 'Jan. 2020', to: 'Mar. 2020' },
+      company: 'ING',
+      role: 'Java Developer',
+      responsibilities: 'Ensure the static reference data team reaches their QBR targets by implementing a cached API and building a generic interface for receiving static data.',
+    },
+    {
+      period: { from: 'Dec. 2017', to: 'Dec. 2019' },
+      company: 'Rabobank',
+      role: 'Devops Engineer',
+      responsibilities: 'Develop maintain and design an application, in a scrum team of 7, which gives the customer insight into their running bank processes. This application was the first to go live on the cloud (Pivotal) and was set up with continuous delivery without downtime.',
+    },
+    {
+      period: { from: 'May 2016', to: 'Nov. 2017' },
+      company: 'EBPI',
+      role: 'Java Developer',
+      responsibilities: 'Develop and maintain, in a scrum team of 5, the platform used to send millions of messages between businesses and the government. As well as implementing the backend services for the governments "BerichtenBox".',
+    },
+    {
+      period: { from: 'Oct. 2014', to: 'Apr. 2016' },
+      company: 'SnappCar',
+      role: 'C# Developer',
+      responsibilities: 'Building and maintaining every aspect of the Snappcar website in a scrum team of 4 including a migration from a local machine to the Amazon cloud.',
     }
+  ],
+  skills: {
+    hard: [
+      { name: 'Java', level: 100 },
+      { name: 'Rust', level: 60 },
+      { name: 'Dart', level: 30 },
+      { name: 'Flutter', level: 40 },
+      { name: 'Truffle suite', level: 30 },
+      { name: 'Blockchain', level: 50 },
+      { name: 'Spring Boot', level: 100 },
+      { name: 'Maven', level: 80 },
+      { name: 'Sonar', level: 80 },
+      { name: 'Jenkins', level: 80 },
+      { name: 'Gitlab', level: 70 },
+      { name: 'CICD', level: 80 },
+      { name: 'Micro services', level: 90 },
+      { name: 'TypeScript', level: 75 },
+      { name: 'Vue.js', level: 75 },
+      { name: 'Flexbox', level: 75 },
+      { name: 'HTML', level: 75 },
+      { name: 'WebdriverIO', level: 70 },
+      { name: 'REST', level: 95 },
+      { name: 'XML', level: 90 },
+      { name: 'SOAP', level: 80 },
+      { name: 'AWS', level: 60 },
+      { name: 'Cloud Foundry', level: 80 },
+      { name: 'SQL', level: 100 },
+      { name: 'Mongo', level: 80 },
+      { name: 'Redis', level: 90 },
+      { name: 'GA', level: 60 },
+      { name: 'Grafana', level: 70 },
+      { name: 'Prometheus', level: 70 },
+      { name: 'Web security', level: 60 },
+    ],
+    soft: [
+      { name: 'Agile', level: 90 },
+      { name: 'Team player', level: 90 },
+      { name: 'Creative', level: 90 }
+    ]
+  }
 }
